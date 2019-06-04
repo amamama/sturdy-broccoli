3f13247d8d492dfb678ec7625cec4fc6b2e856f1
amama <amama@ikiu.me>
2019-06-04 22:58:44 +0900

    impl post_respond but responsing long request gets failure

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ size_t ceiling_pow2(size_t n) {
 	return n;
 }
 
-string alloc_string(char *str) {
+string alloc_string(char const *str) {
 	string ret = {0, 0, NULL};
 	size_t len = strlen(str);
 	if(!len) {
@@ -112,6 +112,48 @@ string concat_string_rawstr(string dest, char *str) {
 string concat_string(string dest, string src) {
 	assert(src.str != NULL);
 	return concat_string_rawstr(dest, src.str);
+}
+
+bool is_leap_year(int year) {
+	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+bool is_valid_day(int year, int month, int day) {
+	return 0 < day && day <= (int[]){31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}[month - 1] + ((month == 2) && is_leap_year(year));
+}
+
+int parse_date_time_format(char const *str) {
+	string s = alloc_string(str);
+	char *full_date = strtok(s.str, "Tt");
+	char *full_time = strtok(NULL, "");
+	debug("full_date = [%s], full_time = [%s]", full_date, full_time);
+	int sscanf_num = -1;
+	int date_fullyear = -1, date_month = -1, date_mday = -1;
+	sscanf_num = sscanf(full_date, "%04d-%02d-%02d", &date_fullyear, &date_month, &date_mday);
+	if(sscanf_num != 3) goto fail;
+	if(date_fullyear < 0) goto fail;
+	if(!(0 < date_month && date_month < 13 && is_valid_day(date_fullyear, date_month, date_mday))) goto fail;
+
+	char *partial_time = strtok(full_time, "Zz+-");
+	char *time_offset = strtok(NULL, "");
+	debug("partial_time = [%s], time_offset = [%s]", partial_time, time_offset);
+	int time_hour = -1, time_minute = -1;
+	double time_second = -1;
+	sscanf_num = sscanf(partial_time, "%02d:%02d:%02lf", &time_hour, &time_minute, &time_second);
+	if(sscanf_num != 3) goto fail;
+	if(!(-1 < time_hour && time_hour < 24 && -1 < time_minute && time_minute < 60 && 0 <= time_second && time_second < 61)) goto fail;
+	if(time_offset[0] == 'Z' && time_offset[1] == 0) goto success;
+	int time_numoffset_hour = -1, time_numoffset_minute = -1;
+	sscanf_num = sscanf(time_offset, "%02d:%02d", &time_numoffset_hour, &time_numoffset_minute);
+	if(!(-1 < time_numoffset_hour && time_numoffset_hour < 24 && -1 < time_numoffset_minute && time_numoffset_minute < 60)) goto fail;
+
+	success:
+	free_string(s);
+	return true;
+
+	fail:
+	free_string(s);
+	return false;
 }
 
 int recv_string(int fd, string *out) {
@@ -170,24 +212,94 @@ static json_t *todo = NULL;
 int init_todo(void) {
 	if(todo) return 0;
 	json_error_t err;
-	todo = json_loads("{\"events\":[]}", 0, &err);
+	todo = json_loads("{\"events\": []}", 0, &err);
 	return 0;
 }
 
+size_t register_todo(json_t *item) {
+	json_t *arr = json_object_get(todo, "events");
+	if(!arr) init_todo();
+	size_t len = json_array_size(arr);
+	json_object_set_new(item, "id", json_integer(len));
+	json_array_append_new(arr, item);
+	return len;
+}
+
+#define HTTP_STATUS_200 "200 OK\r\n"
+#define HTTP_STATUS_400 "400 Bad Request\r\n"
+#define HTTP_STATUS_404 "404 Not Found\r\n"
+#define send_200 (send(fd, HTTP_STATUS_200, strlen(HTTP_STATUS_200), 0))
+#define send_400 (send(fd, HTTP_STATUS_400, strlen(HTTP_STATUS_400), 0))
+#define send_404 (send(fd, HTTP_STATUS_404, strlen(HTTP_STATUS_404), 0))
 int send_todo(int fd) {
+	send_200;
 	return json_dumpfd(todo, fd, 0);
 }
 
-#define HTTP_STATUS_404 "404 Not Found\r\n"
 int get_respond(int id, int fd) {
 	if(id < 0) return send_todo(fd);
 	size_t len = json_array_size(json_object_get(todo, "events"));
-	if(len <= id) return send(fd, HTTP_STATUS_404, strlen(HTTP_STATUS_404), 0);
+	if(len <= id) return send_404;
 	return 0;
 }
 
 int post_respond(string body, int fd) {
+	string message;
+	bool status = true;
+	json_error_t err;
+	json_t *body_json = json_loads(body.str, 0, &err);
+
+	if(!body_json) {
+		debug("error: on line %d: %s\n", err.line, err.text);
+		message = alloc_string("body is not json");
+		status = false;
+	}
+	if(status && !json_is_object(body_json)) {
+		debug("body is not json object\n");
+		message = alloc_string("body is not object");
+		status = false;
+	}
+	json_t *deadline = json_object_get(body_json, "deadline");
+	if(status && !deadline) {
+		debug("json does not have \"deadline\" key\n");
+		message = alloc_string("json does not have \"deadline\" key");
+		status = false;
+	}
+	if(status && !json_is_string(deadline)) {
+		debug("deadline is not string\n");
+		message = alloc_string("deadline is not string");
+		status = false;
+	}
+	if(status && !parse_date_time_format(json_string_value(deadline))) {
+		debug("invalid date format\n");
+		message = alloc_string("invalid date format");
+		status = false;
+	}
+
+	if(status)  {
+		message = alloc_string("registered");
+		send_200;
+	} else {
+		send_400;
+	}
+
+
+	json_t *response = json_object();
+	json_object_set_new(response, "status", json_string(status?"success":"failure"));
+	json_object_set_new(response, "message", json_string(message.str));
+	if(status) {
+		size_t id = register_todo(body_json);
+		json_object_set_new(response, "id", json_integer(id));
+	}
+
+	json_dumpfd(response, fd, 0);
+
+	free_string(message);
+	json_decref(body_json);
+	json_decref(response);
 	return 0;
+
+
 }
 
 int respond(api_t res, int fd) {
@@ -213,8 +325,9 @@ int main(void) {
 		string str = alloc_string("");
 		int nrecv = recv_string(fd, &str);
 		debug("%s", str.str);
-		send_string(fd, str);
+		//send_string(fd, str);
 		respond(parse_HTTP_request(str), fd); //parse overwrites str
+		free_string(str);
 		close(fd);
 	}
 	error(errno, "accept returns -1:\n");
